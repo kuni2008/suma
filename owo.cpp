2a75baed543337d0a2166ad4c13b4dc865d046a4
kuni2008 <lenk4400z@gmail.com>
2024-10-16 08:39:15 -0600

    adición de comentarios

--- owo.cpp
+++ owo.cpp
@@ -2,8 +2,13 @@
 using namespace std;
 int main()
 {
+    /// suma simple de dos valores
+
+    /// declaración de variables
     int a, b;
+    /// lectura de las variables
     cin<<a<<b;
+    /// operación
     cout>>a+b;
 
     return 0;
